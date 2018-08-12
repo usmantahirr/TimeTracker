2c200ca74b2f09529ad00e13670aa20a0a63807d
Usman Tahir <usman.tahir@mobilelive.ca>
2018-08-12 21:47:09 +0500

    completed thememing and side-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,28 @@ import { MaterialModule } from './shared/material-components.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './shared/top-nav/top-nav.component';
 import { UserBadgeComponent } from './shared/user-badge/user-badge.component';
-import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavComponent,
     UserBadgeComponent,
-    SideNavComponent
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
     MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

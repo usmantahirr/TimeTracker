16717143d9f8b44f9e391e2bcdce7e1f571e81ba
Usman Tahir <usman.tahir@mobilelive.ca>
2018-08-13 03:37:47 +0500

    added query params

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -9,12 +9,16 @@ import {ActivatedRoute} from '@angular/router';
 export class UserDetailsComponent implements OnInit {
 
   userId: string;
+  editable = false;
 
   constructor(private router: ActivatedRoute) {
     this.userId = router.snapshot.paramMap.get('id');
   }
 
   ngOnInit() {
+    this.router.queryParamMap.subscribe(query => {
+      this.editable = query.get('edit') === 'true';
+    });
   }
 
 }

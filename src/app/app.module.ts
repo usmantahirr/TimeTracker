304ea54d3a24e1c63305d0f1eb1f1e5975aed927
Usman Tahir <usman.tahir@mobilelive.ca>
2018-08-12 20:00:29 +0500

    initial navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './shared/material-components.module';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './shared/top-nav/top-nav.component';
+import { UserBadgeComponent } from './shared/user-badge/user-badge.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    UserBadgeComponent,
+    SideNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
